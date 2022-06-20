f52d3a40c433d6a0b620c84f6f78503e7081106c
romeolandry <kamgoche@yahoo.com>
2022-06-20 22:58:36 +0200

    add js for side bar

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// sidebar custom
+window.addEventListener("DOMContentLoaded", function() {
+    // var button = document.getElementsByClassName('w3-bar-item');
+    const sidebtns = document.querySelectorAll('.w3-padding-large');
+    for(let i = 0; i < sidebtns.length; i++){
+        sidebtns[i].addEventListener("click", function() {
+            sidebtns[i].classList.toggle("sidebar_clicked");
+
+            for (let j = 0; j < sidebtns.length; j++) {
+                if(i!==j){
+                    sidebtns[j].classList.remove("sidebar_clicked");
+                }            
+            }
+        });
+    }
+  });
